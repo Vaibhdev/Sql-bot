25852744f8f802311de1dd842e65253b6e914dbc
Vaibhdev <157033146+Vaibhdev@users.noreply.github.com>
2025-08-29 12:37:09 +0530

    Update BfhlSqlBotApplication.java

--- sql-bot_springboot/src/main/java/com/example/sqlbot/BfhlSqlBotApplication.java
+++ sql-bot_springboot/src/main/java/com/example/sqlbot/BfhlSqlBotApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BfhlSqlBotApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BfhlSqlBotApplication.class, args);
-    }
+        SpringApplication.run(BfhlSqlBotApplication.class, args);
+    }
 }
